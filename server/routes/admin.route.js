058d4bc645b8ad050e92a05d09ea3f5b7334ef92
NodePulse <bharbeysachin@gmail.com>
2025-08-22 22:35:49 +0530

    changing everything

--- server/routes/admin.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { adminLogin } from "../controllers/admin.controller.js"
-
-const adminRouter = express.Router()
-
-adminRouter.route("/login").post(adminLogin)
-
-export default adminRouter
